1af3708a3e8fcb2db24f9095f99e3d4d0735212b
Quill Henry <henryquill69@gmail.com>
2024-05-10 08:19:26 +0700

    add Home Screen and add Solution screen

--- flutter_application_final/lib/UI/SolutionScreen/solution.dart
+++ flutter_application_final/lib/UI/SolutionScreen/solution.dart
@@ -1,5 +1,4 @@
 
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -14,12 +13,9 @@ class SolutionPage extends StatefulWidget {
 }
 
 class _SolutionPageState extends State<SolutionPage> {
-  var scrollController = ScrollController();
-  int currentIndex = 0;
-  FirebaseAuth? firebaseAuth;
-  late FirebaseFirestore firestore;
-  late Stream<QuerySnapshot> booksStream;
+  late ScrollController scrollController;
 
+  int currentIndex = 0;
   // Dummy list of tips, replace it with your actual list of tips
   List<String> tips = [
     'Practice speaking every day to improve your fluency',
@@ -29,11 +25,80 @@ class _SolutionPageState extends State<SolutionPage> {
     'Test 5',
   ];
 
+  List<Book> books = [
+    Book(
+      id:"3Q4iwZvPBDVahdMCTzMF",
+      image: 'assets/images/book1.png',
+      name: 'Mathematics Extended',
+      price: 22,
+    ),
+    Book(
+      id:"nU6W38aPniHSz0rV6mej",
+      image: 'assets/images/book2.png',
+      name: 'IELTS Test Practice Book 2',
+      price: 10,
+    ),
+    Book(
+      id:"okDw7revv8bh42jjrhlf",
+      image: 'assets/images/book3.png',
+      name: 'Mcmillan English 1',
+      price: 15,
+    ),
+    Book(
+      id:"v9U3l89mlrS5rCVC2HOf",
+      image: 'assets/images/book4.png',
+      name: 'Eloquent JavaScript',
+      price: 20,
+    ),
+    Book(
+      id:"wwwAhLZo9Mnv4nCCtHwt",
+      image: 'assets/images/book5.png',
+      name: 'The Psychology Book',
+      price: 14,
+    ),
+    Book(
+      id:"0RCWNAziKrFrJSPioiGX",
+      image: 'assets/images/book6.png',
+      name: 'Wings of Fire The graphic novel',
+      price: 23,
+    ),
+  ];
+
   @override
   void initState() {
     super.initState();
-    firestore = FirebaseFirestore.instance;
-    booksStream = firestore.collection("books").snapshots();
+    scrollController = ScrollController(initialScrollOffset: 0.0);
+    scrollController.addListener(() {
+      setState(() {
+        currentIndex = (scrollController.offset / 100).round();
+      });
+    });
+    startAutoScroll();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  void startAutoScroll() {
+    Future.delayed(const Duration(seconds: 5), () {
+      if (currentIndex < books.length - 1) {
+        scrollController.animateTo(
+          (currentIndex + 1) * 100.0,
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+        );
+      } else {
+        scrollController.animateTo(
+          0.0,
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+        );
+      }
+      startAutoScroll();
+    });
   }
 
   
@@ -56,51 +121,55 @@ class _SolutionPageState extends State<SolutionPage> {
         child:SingleChildScrollView(
           child: Column(
             children: [    
-
               // book slideshow
               SizedBox(
-                height: 180,
+                height: 210,
                 child: ListView.builder(         
                   controller: scrollController,
                   scrollDirection: Axis.horizontal,
-                  itemCount: tips.length,
+                  itemCount: books.length*2,
                   itemBuilder: (BuildContext context, int index){
+                    int bookIndex = index % books.length;
+                    Book book = books[bookIndex];
                     return Padding(
                       padding: const EdgeInsets.only(right: 8,top: 8,bottom: 8),
                       child: Container(
-                        width: 270, 
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius:BorderRadius.circular(8),
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 0.7,
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 1,
-                              blurRadius: 3,
-                              offset: Offset(0, 3),
+                        width: 100, 
+                        padding: const EdgeInsets.only(right:5),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              book.image,
+                              height: 120,
+                              fit: BoxFit.cover,
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              book.name,
+                              style:  const TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              '\$${book.price}',
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 11,
+                              ),
                             ),
                           ],
-                        ),
-                        child: Center(
-                          child: Text(
-                            tips[index],
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 25,
-                            ),
-                          ),
-                        ),
+                        )
                       ),
                     );
                   },
                 ),
               ),
-              SizedBox(height: 15,),
+              // SizedBox(height: 10,),
 
               // search bar
               PreferredSize(
@@ -163,7 +232,6 @@ class _SolutionPageState extends State<SolutionPage> {
               SizedBox(   
                 height: 180,
                 child: ListView.builder(         
-                  controller: scrollController,
                   scrollDirection: Axis.horizontal,
                   itemCount: tips.length,
                   itemBuilder: (BuildContext context, int index){
@@ -222,7 +290,6 @@ class _SolutionPageState extends State<SolutionPage> {
               SizedBox(
                 height: 180,
                 child: ListView.builder(         
-                  controller: scrollController,
                   scrollDirection: Axis.horizontal,
                   itemCount: tips.length,
                   itemBuilder: (BuildContext context, int index){
--- flutter_application_final/lib/model/book.dart
+++ flutter_application_final/lib/model/book.dart
@@ -1,10 +1,9 @@
-import 'dart:ffi';
 
 class Book{
   String id;
   String image;
   String name;
-  Int price;
+  int price;
 
   Book({required this.id, required this.image, required this.name, required this.price});
   Map<String, dynamic> toJson(){
